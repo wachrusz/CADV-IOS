006e914e9808c4d9de1848102914842b5149a4c7
Mikhail Vakhrushin <122281679+wachrusz@users.noreply.github.com>
2025-02-11 16:43:33 +0300

    Added TCA

--- CADV/CADVApp.swift
+++ CADV/CADVApp.swift
@@ -9,17 +9,23 @@ import RealmSwift
 import FirebaseCore
 import SwiftUI
 import UserNotifications
+import ComposableArchitecture
 
 @main
 struct CADVApp: App {
     @StateObject private var notificationManager = NotificationManager()
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
     var body: some Scene {
         WindowGroup {
-            //RootView()
-            //    .environmentObject(notificationManager)
-            HomeConfigurator.configure()
+            AppView(
+                store: Store(
+                    initialState: AppFeature.State()
+                ) {
+                    AppFeature()
+                }
+            ).environmentObject(notificationManager)
+
+            //RootView().environmentObject(notificationManager)
         }
     }
 }
@@ -45,121 +51,8 @@ struct RootView: View {
                     .preferredColorScheme(.light)
                     .onAppear {
                         notificationManager.requestPermission()
-                        checkToken()
                     }
             }
-        }
-        .onAppear {
-            initializeURLElements()
-        }
-    }
-
-    private func initializeURLElements() {
-        guard Thread.isMainThread else {
-            DispatchQueue.main.async {
-                self.initializeURLElements()
-            }
-            return
-        }
-
-        if let tokenEntity = RealmManager.shared.realm.objects(TokenEntity.self).first {
-            let tokenData = TokenData(
-                accessToken: tokenEntity.accessToken,
-                refreshToken: tokenEntity.refreshToken,
-                accessTokenExpiresAt: tokenEntity.accessTokenExpiresAt,
-                refreshTokenExpiresAt: tokenEntity.refreshTokenExpiresAt
-            )
-            isAuthenticated = isValid(token: tokenData)
-            Logger.shared.log(.info, "URL elements initialized with existing token")
-        } else {
-            Logger.shared.log(.warning, "Realm is empty, creating default token")
-            createDefaultToken()
-            if let tokenEntity = RealmManager.shared.realm.objects(TokenEntity.self).first {
-                let tokenData = TokenData(
-                    accessToken: tokenEntity.accessToken,
-                    refreshToken: tokenEntity.refreshToken,
-                    accessTokenExpiresAt: tokenEntity.accessTokenExpiresAt,
-                    refreshTokenExpiresAt: tokenEntity.refreshTokenExpiresAt
-                )
-                isAuthenticated = false
-                Logger.shared.log(.info, "URL elements initialized with default token")
-            }
-        }
-    }
-
-    private func checkToken(attemptsLeft: Int = 3) {
-        guard Thread.isMainThread else {
-            DispatchQueue.main.async {
-                self.checkToken(attemptsLeft: attemptsLeft)
-            }
-            return
-        }
-        guard !isCheckingToken else { return }
-        isCheckingToken = true
-
-        defer { isCheckingToken = false }
-        Logger.shared.log(.info, "Started checking tokens, attempts left: \(attemptsLeft)")
-
-        let tokenData = URLElements.shared.tokenData
-
-        if isValid(token: tokenData) {
-            Logger.shared.log(.info, "Token is valid")
-            isAuthenticated = true
-            return
-        } else {
-            Logger.shared.log(.warning, "Token is invalid")
-
-            if attemptsLeft > 0 {
-                URLElements.shared.refreshTokenIfNeeded { success in
-                    if success {
-                        Logger.shared.log(.info, "Token refreshed successfully, rechecking...")
-                        self.checkToken(attemptsLeft: attemptsLeft - 1)
-                    } else {
-                        Logger.shared.log(.warning, "Failed to refresh token, attempts left: \(attemptsLeft - 1)")
-                        self.checkToken(attemptsLeft: attemptsLeft - 1)
-                    }
-                }
-            } else {
-                if KeychainHelper.shared.read(forKey: "user_pin_code") != nil {
-                    KeychainHelper.shared.delete(forKey: "user_pin_code")
-                }
-                Logger.shared.log(.error, "All attempts exhausted, authentication failed")
-                isAuthenticated = false
-            }
-        }
-    }
-
-    private func isValid(token: TokenData) -> Bool {
-        Logger.shared.log(.info, "exp: \(token.accessTokenExpiresAt), curr: \(Date())")
-        return token.accessTokenExpiresAt > Date()
-    }
-
-    private func createDefaultToken() {
-        guard Thread.isMainThread else {
-            DispatchQueue.main.async {
-                self.createDefaultToken()
-            }
-            return
-        }
-        RealmManager.shared.realm.deleteAll()
-        if KeychainHelper.shared.read(forKey: "user_pin_code") != nil {
-            KeychainHelper.shared.delete(forKey: "user_pin_code")
-        }
-
-        let tokenEntity = TokenEntity()
-        tokenEntity.deviceID = getDeviceIdentifier()
-        tokenEntity.accessToken = "default_access_token"
-        tokenEntity.refreshToken = "default_refresh_token"
-        tokenEntity.accessTokenExpiresAt = Date().addingTimeInterval(-10000)
-        tokenEntity.refreshTokenExpiresAt = Date().addingTimeInterval(3600)
-
-        do {
-            try RealmManager.shared.realm.write {
-                RealmManager.shared.realm.add(tokenEntity, update: .modified)
-            }
-            Logger.shared.log(.info, "Default token created")
-        } catch {
-            Logger.shared.log(.error, "Failed to create default token: \(error.localizedDescription)")
         }
     }
 }
@@ -183,19 +76,5 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         )
 
         return true
-    }
-}
-
-class RealmManager: ObservableObject {
-    static let shared = RealmManager()
-    let realm: Realm
-
-    private init() {
-        do {
-            self.realm = try Realm()
-            print("Realm created at \(Thread.current)")
-        } catch {
-            fatalError("Failed to initialize Realm: \(error)")
-        }
     }
 }
